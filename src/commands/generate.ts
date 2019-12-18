aca9a61afd97436e3e44fd7a994ea27daf3ff4d9
mkosir <kosir.mar@gmail.com>
2019-12-18 15:21:20 +0100

    refactor flags

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,31 +1,28 @@
 import { Command, flags } from '@oclif/command';
 import cli from 'cli-ux';
+const chalk = require('chalk');
 
 export default class Generate extends Command {
   static description = 'generate documentation';
 
   static flags = {
-    help: flags.help({ char: 'h' }),
+    react: flags.boolean({ description: 'generate StoryTab for React' }),
+
     // flag with a value (-n, --name=VALUE)
     name: flags.string({ char: 'n', description: 'name to print' }),
     // flag with no value (-f, --force)
     force: flags.boolean({ char: 'f' }),
   };
 
-  static args = [{ name: 'file' }];
-
   async run() {
-    cli.action.start('Generating...');
-    await new Promise(res => setTimeout(res, 3000));
+    const { flags } = this.parse(Generate);
 
-    const { args, flags } = this.parse(Generate);
-
-    const name = flags.name || 'world';
-    this.log(
-      `hello ${name} from /Users/mkosir/Desktop/Projects-GitHub/story-tab/src/commands/generate.ts`,
-    );
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+    if (flags.react) {
+      cli.action.start(chalk.blue('Generating Storytab for React...'));
+      await new Promise(res => setTimeout(res, 2000));
+      this.exit();
     }
+
+    this.warn(chalk.yellow('Please provide project option (React, Vue...)'));
   }
 }
