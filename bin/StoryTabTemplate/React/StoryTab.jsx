68d10e5a74f1667433b68dd2a6c0997caa77f871
mkosir <kosir.mar@gmail.com>
2019-12-18 21:54:07 +0100

    remove unused template

--- bin/StoryTabTemplate/React/StoryTab.jsx
+++ bin/StoryTabTemplate/React/StoryTab.jsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
