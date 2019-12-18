818816ff1f2056ee95c9b8d5b31b5e3f2ff6779a
mkosir <kosir.mar@gmail.com>
2019-12-18 16:43:37 +0100

    add StoryTab React component

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,24 +1,22 @@
 import { Command, flags } from '@oclif/command';
-import cli from 'cli-ux';
 const chalk = require('chalk');
+
+import { generateStoryTabReact } from '../StoryTabReact/generateStoryTabReact';
 
 export default class Generate extends Command {
   static description = 'generate documentation';
 
   static flags = {
     react: flags.boolean({ description: 'generate StoryTab for React' }),
-
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
   };
 
   async run() {
     const { flags } = this.parse(Generate);
 
     if (flags.react) {
-      cli.action.start('Generating Storytab for React...');
+      this.log('Generating Storytab for React...');
+      generateStoryTabReact();
+      this.log('StoryTab component was copied to destination');
       await new Promise(res => setTimeout(res, 2000));
       this.exit();
     }
