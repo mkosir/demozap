b147149b900dcebab7a1631d7d6d063c1ba78fdf
mkosir <kosir.mar@gmail.com>
2019-12-20 19:44:41 +0100

    remove StoryTab template (as separate package)

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,7 +1,6 @@
 import { Command, flags } from '@oclif/command';
 const chalk = require('chalk');
 
-import { copyStoryTabTemplate } from '../copyStoryTabTemplate';
 import { findStoryTabFiles } from '../findStoryTabFiles';
 
 export default class Generate extends Command {
@@ -22,11 +21,10 @@ export default class Generate extends Command {
 
   async run() {
     const { args, flags } = this.parse(Generate);
+    console.log('Log: flags.ts', flags.ts);
 
     const frameworkCapitalize = args.framework[0].toUpperCase() + args.framework.slice(1);
     this.log(chalk.blue(`Generating Storytab for ${frameworkCapitalize}...`));
-    await copyStoryTabTemplate(args.framework, flags.ts);
-    this.log(chalk.blue('StoryTab template was copied to destination'));
     const files = findStoryTabFiles();
     console.log('Log: Generate -> run -> files', files);
     this.log(chalk.blue(`Found ${files.length} StoryTab files`));
