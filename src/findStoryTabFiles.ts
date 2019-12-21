559a2ef116d1c8aa66c429f78684d41f6ea0920e
mkosir <kosir.mar@gmail.com>
2019-12-21 18:15:53 +0100

    rename function

--- src/findStoryTabFiles.ts
+++
@@ -1,6 +0,0 @@
-const glob = require('glob');
-
-export const findStoryTabFiles = (): Array<string> => {
-  const storyTabFiles = glob.sync(process.cwd() + '/**/*.storytab.*', {});
-  return storyTabFiles;
-};
