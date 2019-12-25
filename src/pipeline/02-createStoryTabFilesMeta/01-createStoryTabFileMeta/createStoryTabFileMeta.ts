64c468d04f31e7cbafa3e9c45178ad1c135e6989
mkosir <kosir.mar@gmail.com>
2019-12-25 11:14:26 +0100

    refactor project structure

---
+++ src/pipeline/02-createStoryTabFilesMeta/01-createStoryTabFileMeta/createStoryTabFileMeta.ts
@@ -0,0 +1,48 @@
+import { extractStoryTabFilePathInfo } from '../02-extractStoryTabFilePathInfo/extractStoryTabFilePathInfo';
+import { findAssociateStoryTabFilesByName } from '../03-findAssociateStoryTabFilesByName/findAssociateStoryTabFilesByName';
+
+import { StoryTabFileMeta } from '../../../types';
+
+export const createStoryTabFileMeta = (
+  storyTabCodeFilePath: string,
+  storyTabStyleFilePaths: string[],
+  prefix: string,
+): StoryTabFileMeta => {
+  const storyTabCodeFilePathInfo = extractStoryTabFilePathInfo(storyTabCodeFilePath);
+
+  const storyTabComponentFilename = `${prefix}${storyTabCodeFilePathInfo.filename.base}.${storyTabCodeFilePathInfo.filename.ext}`;
+  const storyTabComponentName = `${prefix}${storyTabCodeFilePathInfo.filename.base}`;
+  const storyTabComponentDestinationPath = `${storyTabCodeFilePathInfo.dirname}/${storyTabComponentFilename}`;
+
+  const storyTabFileMeta: StoryTabFileMeta = {
+    path: storyTabComponentDestinationPath,
+    filename: storyTabComponentFilename,
+    componentName: storyTabComponentName,
+    code: {
+      path: storyTabCodeFilePath,
+      dirname: storyTabCodeFilePathInfo.dirname,
+      filename: {
+        base: storyTabCodeFilePathInfo.filename.base,
+        ext: storyTabCodeFilePathInfo.filename.ext,
+      },
+    },
+    style: { path: null, dirname: null, filename: { base: null, ext: null } },
+  };
+
+  const associateStoryTabStyleFilePaths = findAssociateStoryTabFilesByName(
+    storyTabStyleFilePaths,
+    storyTabFileMeta.code.filename.base,
+  );
+  if (associateStoryTabStyleFilePaths) {
+    // per one code file only one style file is possible
+    const storyTabStyleFilePathInfo = extractStoryTabFilePathInfo(
+      associateStoryTabStyleFilePaths[0],
+    );
+    storyTabFileMeta.style.path = associateStoryTabStyleFilePaths[0];
+    storyTabFileMeta.style.dirname = storyTabStyleFilePathInfo.dirname;
+    storyTabFileMeta.style.filename.base = storyTabStyleFilePathInfo.filename.base;
+    storyTabFileMeta.style.filename.ext = storyTabStyleFilePathInfo.filename.ext;
+  }
+
+  return storyTabFileMeta;
+};
--- src/pipeline/02-createStoryTabFilesMeta/createStoryTabFilesMeta.ts
+++ src/pipeline/02-createStoryTabFilesMeta/createStoryTabFilesMeta.ts
@@ -1,50 +1,6 @@
-import { extractStoryTabFilePathInfo } from './01-extractStoryTabFilePathInfo/extractStoryTabFilePathInfo';
-import { findAssociateStoryTabFilesByName } from './02-findAssociateStoryTabFilesByName/findAssociateStoryTabFilesByName';
+import { createStoryTabFileMeta } from './01-createStoryTabFileMeta/createStoryTabFileMeta';
+
 import { StoryTabFileMeta } from '../../types';
-
-const createStoryTabFileMeta = (
-  storyTabCodeFilePath: string,
-  storyTabStyleFilePaths: string[],
-  prefix: string,
-): StoryTabFileMeta => {
-  const storyTabCodeFilePathInfo = extractStoryTabFilePathInfo(storyTabCodeFilePath);
-
-  const storyTabComponentFilename = `${prefix}${storyTabCodeFilePathInfo.filename.base}.${storyTabCodeFilePathInfo.filename.ext}`;
-  const storyTabComponentName = `${prefix}${storyTabCodeFilePathInfo.filename.base}`;
-  const storyTabComponentDestinationPath = `${storyTabCodeFilePathInfo.dirname}/${storyTabComponentFilename}`;
-
-  const storyTabFileMeta: StoryTabFileMeta = {
-    path: storyTabComponentDestinationPath,
-    filename: storyTabComponentFilename,
-    componentName: storyTabComponentName,
-    code: {
-      path: storyTabCodeFilePath,
-      dirname: storyTabCodeFilePathInfo.dirname,
-      filename: {
-        base: storyTabCodeFilePathInfo.filename.base,
-        ext: storyTabCodeFilePathInfo.filename.ext,
-      },
-    },
-    style: { path: null, dirname: null, filename: { base: null, ext: null } },
-  };
-
-  const associateStoryTabStyleFilePaths = findAssociateStoryTabFilesByName(
-    storyTabStyleFilePaths,
-    storyTabFileMeta.code.filename.base,
-  );
-  if (associateStoryTabStyleFilePaths) {
-    // per one code file only one style file is possible
-    const storyTabStyleFilePathInfo = extractStoryTabFilePathInfo(
-      associateStoryTabStyleFilePaths[0],
-    );
-    storyTabFileMeta.style.path = associateStoryTabStyleFilePaths[0];
-    storyTabFileMeta.style.dirname = storyTabStyleFilePathInfo.dirname;
-    storyTabFileMeta.style.filename.base = storyTabStyleFilePathInfo.filename.base;
-    storyTabFileMeta.style.filename.ext = storyTabStyleFilePathInfo.filename.ext;
-  }
-
-  return storyTabFileMeta;
-};
 
 export const createStoryTabFilesMeta = (
   storyTabCodeFilePaths: string[],
