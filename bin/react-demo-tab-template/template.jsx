e5ffcd75e4153b4d10a0008139a2d0a12c02411f
mkosir <kosir.mar@gmail.com>
2020-02-12 22:40:17 +0100

    update DemoTab template

---
+++ bin/react-demo-tab-template/template.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import DemoTab from 'react-demo-tab';
+import Demo from './@FILENAME.demotab';
+
+const code = `@CODE`;
+
+@STYLE
+
+const @COMPONENT_NAME = () => (
+  <DemoTab code={code}@PROP_STYLE@PROP_CODE_EXT@PROP_STYLE_EXT>
+    <Demo />
+  </DemoTab>
+);
+
+export default @COMPONENT_NAME;
