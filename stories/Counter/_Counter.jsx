526034547e1ef466620b4c6485ee1062df45b6f3
mkosir <kosir.mar@gmail.com>
2024-11-02 19:23:14 +0100

    fix(deps): update dependencies

--- stories/Counter/_Counter.jsx
+++ stories/Counter/_Counter.jsx
@@ -3,37 +3,28 @@ import { DemoTab } from 'react-demo-tab';
 
 import Demo from './Counter.demozap';
 
-const code = `import React, { PureComponent } from 'react';
+const code = `import React, { useState } from 'react';
 
-class Counter extends PureComponent {
-  state = {
-    count: 0,
-  };
+const Counter = () => {
+  const [count, setCount] = useState(0);
 
-  onClickHandler = () => {
-    this.setState(({ count }) => ({
-      count: count + 1,
-    }));
-  };
+  const onCountInc = () => setCount(count + 1);
 
-  render() {
-    const { count } = this.state;
-    const msg = \`Button clicked \${count} \${count > 1 ? 'times' : 'time'}\`;
-    const marginBottom = 10;
+  const msg = \`Button clicked \${count.toString()} \${count > 1 ? 'times' : 'time'}\`;
+  const marginBottom = 10;
 
-    return (
-      <>
-        <button
-          style={{ fontSize: 16, padding: '5px 15px', marginBottom: \`\${marginBottom}px\` }}
-          onClick={this.onClickHandler}
-        >
-          Click
-        </button>
-        <div>{msg}</div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <button
+        style={{ fontSize: 16, padding: '5px 15px', marginBottom: \`\${marginBottom.toString()}px\` }}
+        onClick={onCountInc}
+      >
+        Click
+      </button>
+      <div>{msg}</div>
+    </>
+  );
+};
 
 export default Counter;
 `;
